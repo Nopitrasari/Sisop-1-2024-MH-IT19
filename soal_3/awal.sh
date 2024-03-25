b21e67bb229edeba73622039fa5521891f46d427
Benjamin <149749135+Benyaminsan@users.noreply.github.com>
2024-03-25 23:31:30 +0700

    Update awal.sh

--- soal_3/awal.sh
+++ soal_3/awal.sh
@@ -11,12 +11,10 @@ cd genshinNo1_folder
 unzip genshin_character.zip
 for genshin_character; do
     decoded_filename=$(echo "$genshin_character" | xxd -r -p)
-    mv "$genshin_character" "$decoded_filename"
-done 
+    mv "$genshin_character" "$decoded_filename" 
 
 # Baca data karakter dari list_character.csv dan menyesuaikan setiap file ke dalam folder sesuai region tiap karakter
 cat list_character.csv | sed 's/\r$//' | awk -F ',' '{ printf "mv \"%s - %s - %s - %s.jpg\" \"%s - %s - %s - %s.jpg\"\n", $1, $2, $3, $4, $1, $2, $3, $4 }' | bash
 
 # buat folder sesuai setiap region karakternya
-cat list_character.csv | sed 's/\r$//' | awk -F ',' '{ printf "mkdir -p \"%s\"\n", $1 }' | bash
- < list_character.csv
+cat list_character.csv | sed 's/\r$//' | awk -F ',' '{ printf "mkdir -p \"%s\"\n", $1 }' | bash done < list_character.csv
